46a33381926f87893e4481b02175cd2360845a54
hdautovic1 <hdautovic1@etf.unsa.ba>
2020-10-26 18:43:39 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/tutorijal1/Sat.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr.tutorijal1;
+
+public class Sat {
+    private int sati,minute,sekunde;
+    public Sat(int sati, int minute, int sekunde) {Postavi(sati,minute,sekunde);}
+    public void Postavi(int sati, int minute, int sekunde) { this.sati=sati; this.minute=minute; this.sekunde=sekunde; }
+  /*  void Sljedeci() {
+        sekunde++;
+        if (sekunde==60) { sekunde=0; minute++; }
+        if (minute==60) { minute=0; sati++; }
+        if (sati==24) sati=0;
+    }
+    void Prethodni() {
+        sekunde--;
+        if (sekunde==-1) { sekunde=59; minute--; }
+        if (minute==-1) { minute=59; sati--; }
+        if (sati==-1) sati=23;
+    }
+    void PomjeriZa(int pomak) {
+        if (pomak>0) for (int i(0); i<pomak; i++) Sljedeci();
+		else for (int i(0); i<-pomak; i++) Prethodni();
+    }
+    int DajSate() const { return sati; }
+    int DajMinute() const { return minute; }
+    int DajSekunde() const { return sekunde; }
+    void Ispisi() const { cout<<sati<<":"<<minute<<":"<<sekunde<<endl; }
+};*/
+
+
+public static void main(String[] args) {
+	// write your code here
+    }
+}
