73b568d4d1f6d841fc679bb7c275e12c16ac60e2
hdautovic1 <hdautovic1@etf.unsa.ba>
2020-10-26 18:55:29 +0100

    Finalni Commit

--- src/ba/unsa/etf/rpr/tutorijal1/Sat.java
+++ src/ba/unsa/etf/rpr/tutorijal1/Sat.java
@@ -4,30 +4,34 @@ public class Sat {
     private int sati,minute,sekunde;
     public Sat(int sati, int minute, int sekunde) {Postavi(sati,minute,sekunde);}
     public void Postavi(int sati, int minute, int sekunde) { this.sati=sati; this.minute=minute; this.sekunde=sekunde; }
-  /*  void Sljedeci() {
+    public void Sljedeci() {
         sekunde++;
         if (sekunde==60) { sekunde=0; minute++; }
         if (minute==60) { minute=0; sati++; }
         if (sati==24) sati=0;
     }
-    void Prethodni() {
+    public void Prethodni() {
         sekunde--;
         if (sekunde==-1) { sekunde=59; minute--; }
         if (minute==-1) { minute=59; sati--; }
         if (sati==-1) sati=23;
     }
-    void PomjeriZa(int pomak) {
-        if (pomak>0) for (int i(0); i<pomak; i++) Sljedeci();
-		else for (int i(0); i<-pomak; i++) Prethodni();
+    public void PomjeriZa(int pomak) {
+        if (pomak>0) for (int i=0; i<pomak; i++) Sljedeci();
+        else for (int i=0; i<-pomak; i++) Prethodni();
     }
-    int DajSate() const { return sati; }
-    int DajMinute() const { return minute; }
-    int DajSekunde() const { return sekunde; }
-    void Ispisi() const { cout<<sati<<":"<<minute<<":"<<sekunde<<endl; }
-};*/
 
-
-public static void main(String[] args) {
-	// write your code here
+    public int getSati() {
+        return sati;
     }
+    public int getMinute() {
+        return minute;
+    }
+    public int getSekunde() {
+        return sekunde;
+    }
+    public void Ispisi()  {
+        System.out.println(sati + ":" + minute + ":" + sekunde);
+    }
+
 }
